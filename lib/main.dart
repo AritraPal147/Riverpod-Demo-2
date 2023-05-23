5c47a2cdeabcb3b608119fbae6727771f4fdd4a3
Aritra Pal <94114400+AritraPal147@users.noreply.github.com>
2023-05-23 20:30:10 +0530

    Added Increment Button and its functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: 'Flutter Hooks Demo',
-    theme: ThemeData(
-      primarySwatch: Colors.blue,
+  runApp(ProviderScope(
+    child: MaterialApp(
+      title: 'Flutter Hooks Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.dark,
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
     ),
-    darkTheme: ThemeData.dark(),
-    themeMode: ThemeMode.dark,
-    debugShowCheckedModeBanner: false,
-    home: const HomePage(),
   ));
 }
 
@@ -34,14 +37,55 @@ extension OptionalInfixAddition<T extends num> on T? {
   }
 }
 
-class HomePage extends StatelessWidget {
+class Counter extends StateNotifier<int?> {
+  /// Constructor for [StateNotifier]
+  Counter() : super(null);
+
+  /// Increments value of [state]
+  ///
+  /// If [state] is null (initial state), sets [state] = 1
+  /// otherwise, increases value of state by 1
+  void increment() => state = state == null ? 1 : state + 1;
+}
+
+/// Creates a [StateNotifierProvider] based on our [Counter] class, which is
+/// a [StateNotifier]
+final counterProvider = StateNotifierProvider<Counter, int?>(
+  (ref) => Counter(),
+);
+
+class HomePage extends ConsumerWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  /// [ref] provides a reference to our provider
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Page'),
+        title: Consumer(
+          /// There is a [ref] within the [Consumer] widget because this ref allows
+          /// only the [Consumer] widget and its children to be rebuilt
+          /// and not the entire [HomePage] and all its children as it is waste of resources
+          builder: (BuildContext context, WidgetRef ref, Widget? child) {
+            final count = ref.watch(counterProvider);
+            final text =
+                count == null ? 'Press the button' : count.toString();
+            return Center(child: Text(text));
+          },
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          TextButton(
+
+              /// ref.read() allows us to get a snapshot of the object which is passed
+              /// which allows us to call the increment function.
+              ///
+              /// Since increment() is a void function, it can also be written as below
+              onPressed: ref.read(counterProvider.notifier).increment,
+              child: const Text('Increment Counter')),
+        ],
       ),
     );
   }
